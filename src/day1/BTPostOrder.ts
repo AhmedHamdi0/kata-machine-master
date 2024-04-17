dd37d2e1ed133f26aef69d9ba803c80f121d7e15
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-18 00:01:44 +0200

    implement Binary Tree`PostOrder` traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,2 +1,18 @@
+function traverse(
+    currentNode: BinaryNode<number> | null,
+    path: number[],
+): number[] {
+    if (!currentNode) {
+        return path;
+    }
+
+    traverse(currentNode.left, path);
+    traverse(currentNode.right, path);
+    path.push(currentNode.value);
+
+    return path;
+}
+
 export default function post_order_search(head: BinaryNode<number>): number[] {
+    return traverse(head, []);
 }
