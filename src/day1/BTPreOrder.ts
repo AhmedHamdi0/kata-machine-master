eb0b51cccb3ad62bb6a4e65ba669c0ea4ff67c07
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-18 00:01:31 +0200

    implement Binary Tree`PreOrder` traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,2 +1,18 @@
+function traverse(
+    currentNode: BinaryNode<number> | null,
+    path: number[],
+): number[] {
+    if (!currentNode) {
+        return path;
+    }
+
+    path.push(currentNode.value);
+    traverse(currentNode.left, path);
+    traverse(currentNode.right, path);
+
+    return path;
+}
+
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return traverse(head, []);
 }
