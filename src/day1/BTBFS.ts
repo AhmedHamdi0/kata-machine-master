2edc3283835baf057e28ca6ed3c73642613f5464
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-18 00:02:12 +0200

    implement Binary Tree`Breadth First Search` traversal

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,2 +1,23 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const queue: (BinaryNode<number> | null)[] = [head];
+
+    while (queue.length) {
+        const currentNode = queue.shift() as
+            | BinaryNode<number>
+            | null
+            | undefined;
+
+        if (!currentNode) {
+            continue;
+        }
+
+        if (currentNode.value === needle) {
+            return true;
+        }
+
+        queue.push(currentNode.left);
+        queue.push(currentNode.right);
+    }
+
+    return false;
 }
