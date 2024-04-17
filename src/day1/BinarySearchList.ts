40119a482f85556d6bedca436945db42fd4efae8
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-17 23:57:43 +0200

    implement `binary search` algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,2 +1,20 @@
 export default function binary_search(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        const mid_index = Math.floor(low + (high - low) / 2);
+        const mid_value = haystack[mid_index];
+
+        if (mid_value === needle) {
+            return true;
+        } else if (mid_value > needle) {
+            high = mid_index
+        } else {
+            low = mid_index + 1
+        }
+
+    } while(low < high);
+
+    return false;
 }
