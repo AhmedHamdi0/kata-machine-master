014314764bff945beca8ee65c156edd0c5b127ef
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-18 00:01:39 +0200

    implement Binary Tree`InOrder` traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,2 +1,18 @@
+function traverse(
+    currentNode: BinaryNode<number> | null,
+    path: number[],
+): number[] {
+    if (!currentNode) {
+        return path;
+    }
+
+    traverse(currentNode.left, path);
+    path.push(currentNode.value);
+    traverse(currentNode.right, path);
+
+    return path;
+}
+
 export default function in_order_search(head: BinaryNode<number>): number[] {
+    return traverse(head, []);
 }
