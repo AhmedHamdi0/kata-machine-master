d7103c3439f6bc5aa565f5333ca07b2485463e4a
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-17 23:59:29 +0200

    implement `Queue` data structure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,14 +1,47 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
+    private head?: Node<T>;
+    private tail?: Node<T>;
+    public length: number;
+
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const newNode = { value: item } as Node<T>;
+        this.length++;
+
+        if (!this.tail) {
+            this.head = this.tail = newNode;
+            return;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
     }
 
     deque(): T | undefined {
         if (!this.head) {
+            return undefined;
+        }
+
+        this.length--;
+
+        if (this.length === 0) this.tail = undefined;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        return head.value;
     }
 
     peek(): T | undefined {
+        return this.head?.value;
     }
 }
