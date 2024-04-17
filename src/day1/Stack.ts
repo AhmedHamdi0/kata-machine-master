93c5dfeddb55abc060e88a82e56884f2e96aa9c6
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-17 23:59:46 +0200

    implement `Stack` data structure

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,10 +1,44 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
+    public length: number;
+    private head?: Node<T>;
+
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const newNode = { value: item } as Node<T>;
+
+        this.length++;
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        newNode.prev = this.head;
+        this.head = newNode;
     }
 
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        const head = this.head as Node<T>;
+
+        if (this.length === 0) {
+            this.head = undefined;
+        } else {
+            this.head = head.prev;
+        }
+
+        return head?.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
     }
 }
